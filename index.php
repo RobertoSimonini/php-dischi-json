b452441948842bebdcd87766d569a44bb6b4f19b
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-09 17:30:39 +0100

    created filter for genre

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-
 $url = __DIR__ . '/data.json';
 
 $json_data = file_get_contents($url);
+$discs = json_decode($json_data, true);
 
-$discs = $json_data;
+$genre = $_GET['genre'] ?? '';
+
+if (!empty($genre)) {
+
+    $searched_genre = strtolower($genre);
+    $filtered_discs = [];
+
+    foreach ($discs as $disc) {
+        $music_genre = strtolower($disc['genre']);
+        if (str_contains($music_genre, $searched_genre)) $filtered_discs[] = $disc;
+    }
+
+    $discs = $filtered_discs;
+}
 
 header('Content-type: application/json');
-echo ($discs);
+echo json_encode($discs);
